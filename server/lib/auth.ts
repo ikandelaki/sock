2da256e2123676615c743e4b1ed4188124bb15aa
ikandelaki <iraklikandelaki2@gmail.com>
2026-05-07 17:34:51 +0400

    Create a refresh token model and utilities to create and delete refresh tokens based on token string and user id

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import { prisma } from "./prisma";
 
 export const tokenExpiration = {
   access_token: "7d",
@@ -11,10 +12,47 @@ export enum TokenType {
 }
 
 const secret = process.env["JWT_SECRET"]!;
+const refreshSecret = process.env["JWT_REFRESH_SECRET"]!;
 const expiresIn = process.env["JWT_EXPIRES_IN"]!;
+const refreshExpiresIn = process.env["JWT_REFRESH_EXPIRES_IN"];
 
 export const generateAccessToken = (userId: number) => {
   const accessToken = jwt.sign({ userId }, secret, { expiresIn });
 
   return accessToken;
+};
+
+export const generateRefreshToken = (userId: number) => {
+  const refreshToken = jwt.sign({ userId }, refreshSecret, {
+    expiresIn: refreshExpiresIn,
+  });
+
+  return refreshToken;
+};
+
+export const saveRefreshToken = async (userId: number, token: string) => {
+  const expiresAt = new Date();
+  expiresAt.setDate(expiresAt.getDate() + 30);
+
+  const refreshToken = await prisma.refreshToken.create({
+    data: { token, userId, expiresAt },
+  });
+
+  return refreshToken;
+};
+
+export const revokeRefreshTOken = async (token: string) => {
+  await prisma.refreshToken.deleteMany({
+    where: {
+      token,
+    },
+  });
+};
+
+export const revokeAllUserTokens = async (userId: number) => {
+  await prisma.refreshToken.deleteMany({
+    where: {
+      userId,
+    },
+  });
 };
